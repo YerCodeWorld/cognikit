6b042b545870dd58cd46d203318fd2f21a21301e
Yahir Adolfo de los Santos Beras <yahiradolfo3@gmail.com>
2025-12-27 08:49:56 -0400

    sketch: skeleton functions for association module (parser, validator and grader)

---
+++ src/modules/association/implementation.ts
@@ -0,0 +1,11 @@
+import { AssociationData } from "../../shared/types";
+
+// returns AssociationData
+export function associationDataParser(code: string) {}
+
+export function associationDataValidator(data: AssociationData ): boolean {
+	return false;
+}
+
+// Return ScoreResult
+export function associationDataGrader() {}
